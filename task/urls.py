f9dfafb70b8aa15da6939519f0c95e3845beb9a7
faaaaaa6 <fahanasmuhammad634@gmail.com>
2026-06-11 14:58:32 +0530

    added TaskDetail view update and delete

--- task/urls.py
+++ task/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskList,TaskDetail
 
 urlpatterns = [
     path("tasks/",TaskList.as_view(), name='tasks'),
+    #<in:pk>/ uses for retrieve the specific object in database ,pk =primary key
+    path('tasks/<int:pk>/',TaskDetail.as_view(), name='task-detail'),
 ]
--- task/views.py
+++ task/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Task #for fetch the data from the database
 from .serializers import TaskSerializer
 
+#used this task for (create(POST)/read(GET)) for the user
 class TaskList(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = TaskSerializer
@@ -13,3 +14,11 @@ class TaskList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+# used this task for (update(PUT)/delete(DELETE))
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    serilaizer_class = TaskSerializer
+
+    def get_queryset(self):
+        return Task>object.filter(user=self.request.user)
+        
